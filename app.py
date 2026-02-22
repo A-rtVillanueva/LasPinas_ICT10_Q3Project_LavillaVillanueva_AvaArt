e80c0cf795950b2c4226360334ea9ec3896fd94f
A-rtVillanueva <villanueva.av@obmontessori.edu.ph>
2026-02-22 18:53:22 +0800

    msg

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from datetime import datetime
 import json
@@ -377,6 +377,14 @@ def get_players():
         'total': len(all_players)
     }), 200
 
+@app.route('/')
+def serve_index():
+    return send_from_directory('.', 'index.html')
 
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('.', filename)
+    
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
